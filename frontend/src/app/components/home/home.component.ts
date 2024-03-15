2c407a9670b5246e36783fd46a94537a0dfc66bc
David <davidcamposg23@gmail.com>
2024-03-15 17:05:53 -0300

    feat: style da pagina home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -23,4 +23,22 @@ export class HomeComponent implements OnInit{
     this.data = data;
     });
   }
+
+
+  formatDate(dateString : any) {
+    const date = new Date(dateString);
+            const day = date.getDate();
+            const month = date.getMonth() + 1; // Mês é baseado em zero, então adicionamos 1
+            const year = date.getFullYear();
+            const hour = date.getHours();
+            const minute = date.getMinutes();
+
+            // Certifique-se de que o dia, mês, hora e minuto tenham dois dígitos
+            const formattedDay = day < 10 ? '0' + day : day;
+            const formattedMonth = month < 10 ? '0' + month : month;
+            const formattedHour = hour < 10 ? '0' + hour : hour;
+            const formattedMinute = minute < 10 ? '0' + minute : minute;
+
+            return `${formattedDay}/${formattedMonth}/${year} ${formattedHour}:${formattedMinute}`;
+}
 }
